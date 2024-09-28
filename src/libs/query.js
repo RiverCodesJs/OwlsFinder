b5df3505ceb5f1eb805a398212ecc63384edec62
Salvador511 <salvadororozcogon511@gmail.com>
2024-09-28 11:27:42 -0600

    feat: added conectionOrCreation feat to the libs

--- src/libs/query.js
+++ src/libs/query.js
@@ -1,21 +1,23 @@
 import db from '~/libs/db'
 import payloadFormatter from '~/utils/payloadFormatter'
 
-const opts = (filter, includes, data) => {
+const opts = (filter, includes, data, relationEntity, relationData) => {
   const filters = { where: { ...filter } }
   const include = includes ? { 
     include: includes.reduce((acc, include) => ({ ...acc, [include]: { select: { id: true } } }), {}) 
   } : {}
   const datas = data ? { data } : {}
+  const connection = { [relationEntity]: { connectOrCreate: { where: { id: relationData.id || 0 }, create: { ...relationData } } } }
 
-  const obj = Object.assign(filters, include, datas)
+  const obj = Object.assign(filters, include, datas, connection)
   
   return obj
 }
+
 //@queryType one of [findUnique, findMany, delete, update, create]
-const query = async ({ entity, filter, includes, queryType, data }) => {
+const query = async ({ entity, filter, includes, queryType, data, relationEntity, relationData }) => {
   
-  const options = opts(filter, includes, data)
+  const options = opts(filter, includes, data, relationEntity, relationData)
 
   let payload
   switch(queryType){
@@ -46,13 +48,3 @@ const query = async ({ entity, filter, includes, queryType, data }) => {
 }
 
 export default query
-
-// const obj = {
-//   a: 'a',
-//   b: {
-//     name:'letter b',
-//     action: () => 'hello'
-//   }
-// }
-
-// console.log(obj['b']['action']())
