7a2f1d2d4cf0a480a831bfb660dd3ece733e29d0
Salvador511 <salvadororozcogon511@gmail.com>
2024-09-28 12:40:17 -0600

    feat: added many connectOrCreate and special include for permissions. Changed the name of filter to cleanerData

--- src/libs/cleanerData.js
+++ src/libs/cleanerData.js
@@ -1,7 +1,7 @@
 import * as R from 'ramda'
 
-const filter = obj => {
+const cleanerData = obj => {
   return R.omit(['password', 'created_at', 'updated_at', 'active'], obj)
 }
 
-export default filter
+export default cleanerData
--- src/libs/query.js
+++ src/libs/query.js
@@ -1,45 +1,62 @@
 import db from '~/libs/db'
 import payloadFormatter from '~/utils/payloadFormatter'
+import cleanerData from '~/libs/cleanerData'
 
-const opts = (filter, includes, data, relationEntity, relationData) => {
-  const filters = { where: { ...filter } }
+const getOptions = ({ filter, includes, payload: p, relations }) => {
+  const filters = filter ? { where: { ...filter } } : {}
   const include = includes ? { 
-    include: includes.reduce((acc, include) => ({ ...acc, [include]: { select: { id: true } } }), {}) 
+    include: includes.reduce((acc, include) => ({ 
+      ...acc, 
+      [include]: { 
+        select: include ==='permissions' ? { name: true } : { id: true } 
+      } 
+    }), {}) 
   } : {}
-  const datas = data ? { data } : {}
-  const connection = { [relationEntity]: { connectOrCreate: { where: { id: relationData.id || 0 }, create: { ...relationData } } } }
+  const payload = p ? { data: { ...p } } : {}
+  const connections = relations ? relations.reduce((acc, relation) => {
+    const { entity, data } = relation
+    return {
+      ...acc,
+      [entity]: {
+        connectOrCreate: data.map(relationData => ({
+          where: { id: relationData.id || 0 },
+          create: { ...relationData }
+        }))
+      }
+    }
+  }, {}) : {}
 
-  const obj = Object.assign(filters, include, datas, connection)
+  const obj = Object.assign(filters, include, payload, connections)
   
   return obj
 }
 
 //@queryType one of [findUnique, findMany, delete, update, create]
-const query = async ({ entity, filter, includes, queryType, data, relationEntity, relationData }) => {
+const query = async ({ entity, filter, includes, queryType, data, relations }) => {
   
-  const options = opts(filter, includes, data, relationEntity, relationData)
+  const opts = getOptions(filter, includes, data, relations)
 
   let payload
   switch(queryType){
     case 'findUnique':
-      payload = payloadFormatter(await db[entity].findUnique({ ...options }))
+      payload = payloadFormatter(cleanerData(await db[entity].findUnique({ ...opts })))
       return payload
 
     case 'findMany':
-      payload = payloadFormatter(await db[entity].findMany({ ...options }))
+      payload = payloadFormatter(cleanerData(await db[entity].findMany({ ...opts })))
       return payload
       
 
     case 'create':
-      payload = payloadFormatter(await db[entity].create({ ...options }))
+      payload = payloadFormatter(cleanerData(await db[entity].create({ ...opts })))
       return payload
     
     case 'update':
-      payload = payloadFormatter(await db[entity].update({ ...options }))
+      payload = payloadFormatter(cleanerData(await db[entity].update({ ...opts })))
       return payload
     
     case 'delete':
-      payload = payloadFormatter(await db[entity].delete({ ...options }))
+      payload = payloadFormatter(cleanerData(await db[entity].delete({ ...opts })))
       return payload
     
     default: 
