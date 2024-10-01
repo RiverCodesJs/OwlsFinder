d3b222fae3102d0a48d369e2e2eaad8768ec85e2
Salvador511 <salvadororozcogon511@gmail.com>
2024-09-30 19:11:58 -0600

    fix: adapted endpoint to the new libs

--- src/app/api/package/[id]/route.js
+++ src/app/api/package/[id]/route.js
@@ -1,18 +1,19 @@
 import { NextResponse } from 'next/server'
-import db from '~/libs/db'
-import filter from '~/libs/filter'
+import query from '~/libs/query'
 
 export const GET = async (request, { params }) => {
   const { id } = params
 
   try{
-    const packageFound = await db.package.findUnique({
-      where: { id: Number(id) },
-    })
+    const params = {
+      entity: 'package',
+      queryType: 'findUnique',
+      includes: ['subjects'],
+      filter: { id: Number(id) }
+    }
+    const pack = await query({ ...params })
 
-    const pack = filter(packageFound)
-
-    return NextResponse.json( pack, { status: 200 })
+    return NextResponse.json(pack, { status: 200 })
   } catch (error) {
     console.error('Error fetching package:', error)
     return NextResponse.json({ error: 'Error fetching package' }, { status: 500 })
@@ -21,15 +22,30 @@ export const GET = async (request, { params }) => {
 
 export const PUT = async (request, { params }) => {
   const { id } = params
-  const packageData = await request.json()
-  
-  try{
-    const pack = await db.package.find({
-      where:{ id: Number(id) },
-      data:{ ...packageData }
-    })
 
-    return NextResponse.json({ pack }, { status: 200 })
+  const { name, description, images, videos, limit, subjects } = await request.json()
+
+  if(!name || !description || !images || !videos || !limit || !subjects){
+    return NextResponse.json({ error: 'Invalid input' }, { status: 400 })
+  }
+
+  try{
+    const params = {
+      entity: 'package',
+      queryType: 'update',
+      includes: ['subjects'],
+      filter: { id: Number(id) },
+      data: {
+        name,
+        description,
+        images,
+        videos,
+        limit,
+      }
+    }
+    const pack = await query({ ...params })
+
+    return NextResponse.json(pack, { status: 200 })
   } catch (error) {
     console.error('Error updating package:', error)
     return NextResponse.json({ error: 'Error updating package' }, { status: 500 })
@@ -41,12 +57,16 @@ export const PATCH = async (request, { params }) => {
   const partialUpdate = await request.json()
 
   try {
-    const pack = await db.package.update({
-      where: { id: Number(id) },
+    const params = {
+      entity: 'package',
+      queryType: 'update',
+      includes: ['subjects'],
+      filter: { id: Number(id) },
       data: { ...partialUpdate }
-    })
+    }
+    const pack = await query({ ...params })
 
-    return NextResponse.json({ pack }, { status: 200 })
+    return NextResponse.json(pack, { status: 200 })
   } catch (error) {
     console.error('Error updating package partially:', error)
     return NextResponse.json({ error: 'Error updating package partially' }, { status: 500 })
@@ -58,9 +78,16 @@ export const DELETE = async (request, { params }) => {
   const { id } = params
 
   try {
-    await db.package.delete({ where: { id } }) 
+    const params = {
+      entity: 'package',
+      queryType: 'delete',
+      includes: ['subjects'],
+      filter: { id: Number(id) },
+    }
+
+    const pack = await query({ ...params })
     
-    return NextResponse.json({ message: 'Deleting package successfully' }, { status: 200 })
+    return NextResponse.json(pack, { message: 'Deleting package successfully' }, { status: 200 })
   } catch (error) {
     console.error('Error deleting package:', error)
     return NextResponse.json({ error: 'Error deleting package' }, { status: 500 })
--- src/app/api/package/route.js
+++ src/app/api/package/route.js
@@ -1,31 +1,35 @@
 import { NextResponse } from 'next/server'
-import db from '~/libs/db'
-import filter from '~/libs/filter'
+import query from '~/libs/query'
 
 export const POST = async request => {
   try {
     const { name, description, images, videos, limit, subjects } = await request.json()
 
-    const newPackage = await db.package.create({
+    if(!name || !description || !images || !videos || !limit || !subjects){
+      return NextResponse.json({ error: 'Invalid input' }, { status: 400 })
+    }
+
+    const params = {
+      entity: 'package',
+      queryType: 'create',
+      includes: ['subjects'],
       data: {
         name,
         description,
         images,
         videos,
         limit,
-        subjects:{
-          connectOrCreate: subjects.map( subject => ({
-            where: { id: subject.id || 0 },
-            create:{
-              name: subject.name,
-              description: subject.description
-            }
-          }))
-        }
-      }
-    })
+      },
+      relations: [{
+        entity: 'subjects',
+        data: subjects
+      }]
+    }
 
-    return NextResponse.json({ message: 'Package created successfully', newPackage }, { status: 201 })
+
+    const newPackage = await query({ ...params })
+
+    return NextResponse.json(newPackage, { status: 201 })
   } catch (error) {
     console.error('Package creation failed:', error)
     return NextResponse.json({ error: 'Package creation failed' }, { status: 500 })
@@ -34,12 +38,16 @@ export const POST = async request => {
 
 export const GET = async () => {
   try{
-  
-    const packagesFound = await db.package.findMany()
 
-    const packages = packagesFound.map(pack => filter(pack))
+    const params = {
+      entity: 'package',
+      queryType: 'findMany',
+      includes: ['subjects']
+    }
 
-    return NextResponse.json( packages, { status: 200 })
+    const packages = await query({ ...params })
+
+    return NextResponse.json(packages, { status: 200 })
   } catch (error) {
     console.error('Error fetching packages:', error)
     return NextResponse.json({ error: 'Error fetching packages' }, { status: 500 })
