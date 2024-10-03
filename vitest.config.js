614ff8cb2c4bb9b41d43b49747cbcb254f97cb65
Salvador511 <salvadororozcogon511@gmail.com>
2024-10-03 13:38:58 -0600

    Fix: adecuated vitest config to use alias and the setupfiles

--- vitest.config.js
+++ vitest.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import path from 'path'
  
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '~', replacement: path.resolve(__dirname, 'src') },
+    ],
+  },
   test: {
+    globals: true,
     environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
     coverage: {
       provider: 'v8', 
       reportsDirectory: './coverage',
