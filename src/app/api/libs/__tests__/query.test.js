4c07ccbef743f29d5b5cbebe641d8e491279bd4a
Salvador511 <salvadororozcogon511@gmail.com>
2024-10-08 11:22:52 -0600

    feat: test of query

---
+++ src/app/api/libs/__tests__/query.test.js
@@ -0,0 +1,175 @@
+import { describe, it, expect, vi } from 'vitest'
+import query from '~/app/api/libs/query'
+
+vi.mock('~/app/api/libs/db', () => {
+  return {
+    default: {
+      package: {
+        findMany: () => ([
+          { 
+            id: 1,
+            name: 'package1',
+            subjects: [{ id: 1 }, { id: 2 }],
+            created_at: 'created_at',
+            updated_at: 'updated_at',
+            active: 'active'
+            
+          },
+          {
+            id : 2,
+            name: 'package2',
+            subjects: [{ id: 1 }, { id: 2 }] ,
+            created_at: 'created_at',
+            updated_at: 'updated_at',
+            active: 'active'
+          }
+        ]),
+
+        findUnique: ({ where }) => ({ 
+          id: where.id, 
+          name: 'package1', 
+          subjects: [{ id: 1 }, { id: 2 }], 
+          created_at: 'created_at',
+          updated_at: 'updated_at',
+          active: 'active'
+        }),
+        
+        create: ({ data }) => ({
+          id: 1, 
+          name: data.name,
+          subjects: [{ id: 1 }, { id:2 }],
+          created_at: 'created_at',
+          updated_at: 'updated_at',
+          active: 'active'
+        }),
+
+        update:  ({ where, data }) => ({
+          id: where.id,
+          name: data.name,
+          subjects: [{ id: 1 }, { id: 2 }],
+          created_at: 'created_at',
+          updated_at: 'updated_at',
+          active: 'active'
+        }),
+
+        delete: ({ where }) => ({
+          id: where.id,
+          name: 'package1',
+          subjects: [{ id: 1 }, { id: 2 }],
+          created_at: 'created_at',
+          updated_at: 'updated_at',
+          active: 'active'
+        })
+      }
+    } 
+  }
+})
+
+describe('query libs', () =>{
+  it.each([
+    {
+      descr: 'Empty data',
+      entity: null,
+      queryType: null,
+      filter: null, 
+      includes: null, 
+      data: null, 
+      relations: null,
+      result: null
+    },
+    {
+      descr: 'Find Many case',
+      entity: 'package',
+      queryType: 'findMany',
+      filter: null, 
+      includes: ['subjects'], 
+      data: null, 
+      relations: null,
+      result: {
+        1: { 
+          id: 1, 
+          name: 'package1', 
+          subjects: [1, 2] 
+        }, 
+        2: { 
+          id: 2, 
+          name: 'package2', 
+          subjects: [1, 2] 
+        } 
+      },
+    },
+    {
+      descr: 'Find Unique case',
+      entity: 'package',
+      queryType: 'findUnique',
+      filter: { id: 1 }, 
+      includes: ['subjects'], 
+      data: null, 
+      relations: null,
+      result: {
+        id: 1, 
+        name: 'package1', 
+        subjects: [1, 2] 
+      },
+    },
+    {
+      descr: 'Create case',
+      entity: 'package',
+      queryType: 'create',
+      filter: null, 
+      includes: ['subjects'], 
+      data: {
+        name: 'package1'
+      }, 
+      relations: [{
+        entity: 'subjects',
+        data: [
+          {
+            id: 1,
+            name: 'subject1'
+          },
+          {
+            name: 'subject2'
+          }
+        ]
+      }],
+      result: {
+        id: 1, 
+        name: 'package1', 
+        subjects: [1, 2] 
+      },
+    },
+    {
+      descr: 'Update case',
+      entity: 'package',
+      queryType: 'update',
+      filter: { id: 1 }, 
+      includes: ['subjects'], 
+      data: {
+        name: 'packageUpdated'
+      }, 
+      relations: null,
+      result: {
+        id: 1, 
+        name: 'packageUpdated', 
+        subjects: [1, 2] 
+      }
+    },
+    {
+      descr: 'Delete case',
+      entity: 'package',
+      queryType: 'delete',
+      filter: { id: 1 }, 
+      includes: ['subjects'], 
+      data: null,
+      relations: null,
+      result: {
+        id: 1, 
+        name: 'package1', 
+        subjects: [1, 2] 
+      }
+    }
+  ])('$descr', async ({ result, ...props }) => {
+    expect(await query(props)).toEqual(result)
+  })
+})
