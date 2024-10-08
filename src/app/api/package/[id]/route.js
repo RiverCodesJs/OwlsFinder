7ffa3ca5e97ddf4297b2750832f6c33c777c9e04
Salvador511 <salvadororozcogon511@gmail.com>
2024-10-08 11:20:16 -0600

    Fix: adapted the endpoint to the new necessities

--- src/app/api/package/[id]/route.js
+++ src/app/api/package/[id]/route.js
@@ -1,5 +1,7 @@
 import { NextResponse } from 'next/server'
-import query from '~/libs/query'
+import query from '~/app/api/libs/query'
+import { packageShape } from '~/app/api/utils/shapes'
+import { equals } from 'ramda'
 
 export const GET = async (request, { params }) => {
   const { id } = params
@@ -8,100 +10,151 @@ export const GET = async (request, { params }) => {
     const params = {
       entity: 'package',
       queryType: 'findUnique',
-      includes: ['subjects'],
       filter: { id: Number(id) }
     }
-    const pack = await query({ ...params })
+    const pack = await query(params)
 
     return NextResponse.json(pack, { status: 200 })
   } catch (error) {
     console.error('Error fetching package:', error)
-    return NextResponse.json({ error: 'Error fetching package' }, { status: 500 })
+    return NextResponse.json({ error: error.message }, { status: 500 })
   }
 }
 
 export const PUT = async (request, { params }) => {
   const { id } = params
 
-  const { name, description, images, videos, limit, subjects } = await request.json()
-
-  if(!name || !description || !images || !videos || !limit){
-    return NextResponse.json({ error: 'Invalid input' }, { status: 400 })
-  }
+  const data = await request.json()
 
   try{
+    if (!packageShape().every(key => key in data)) {
+      return NextResponse.json({ error: 'Invalid input' }, { status: 400 })
+    }
+
+    for (let i = 1; i <= 3; i++) {
+      const subjectKey = `subject${i}`
+
+      if (!data[subjectKey].id) {
+        const subjectParams = {
+          entity: 'subject',
+          queryType: 'create',
+          data: data[subjectKey] 
+        }
+        
+        const newSubject = await query(subjectParams)
+        data[subjectKey] = newSubject.id 
+
+      }else{
+        const subjectParams = {
+          entity: 'subject',
+          queryType: 'findUnique',
+          filter: { id: data[subjectKey].id },
+        }
+
+        const currentSubject = await query(subjectParams)
+
+        if(!equals(currentSubject, data[subjectKey])){
+          const updateParams = {
+            entity: 'subject',
+            queryType: 'update',
+            filter: { id: data[subjectKey].id },
+            data: data[subjectKey]
+          }
+          await query(updateParams)
+        }
+
+        data[subjectKey] = data[subjectKey].id 
+      }
+    }
+
     const params = {
       entity: 'package',
       queryType: 'update',
-      includes: ['subjects'],
       filter: { id: Number(id) },
-      data: {
-        name,
-        description,
-        images,
-        videos,
-        limit,
-      },
-      relations:[{
-        entity: 'subjects',
-        data: subjects
-      }]
+      data
     }
-    const pack = await query({ ...params })
+    const pack = await query(params)
 
     return NextResponse.json(pack, { status: 200 })
   } catch (error) {
     console.error('Error updating package:', error)
-    return NextResponse.json({ error: 'Error updating package' }, { status: 500 })
+    return NextResponse.json({ error: error.message }, { status: 500 })
   }
 }
 
 export const PATCH = async (request, { params }) => {
   const { id } = params
-  const { subjects, ...partialUpdate } = await request.json()
-
-  const relations = subjects ? ( {
-    relations: [{
-      entity: 'subjects',
-      data: subjects
-    }]
-  }) : undefined
+  const data = await request.json()
 
   try {
+    for (let i = 1; i <= 3; i++) {
+      const subjectKey = `subject${i}`
+
+      if (data[subjectKey] && !data[subjectKey].id) {
+        const subjectParams = {
+          entity: 'subject',
+          queryType: 'create',
+          data: data[subjectKey] 
+        }
+        
+        const newSubject = await query(subjectParams)
+        data[subjectKey] = newSubject.id 
+
+      }else if(data[subjectKey]){
+        const subjectParams = {
+          entity: 'subject',
+          queryType: 'findUnique',
+          filter: { id: data[subjectKey].id },
+        }
+
+        const currentSubject = await query(subjectParams)
+
+        if(!equals(currentSubject, data[subjectKey])){
+          const updateParams = {
+            entity: 'subject',
+            queryType: 'update',
+            filter: { id: data[subjectKey].id },
+            data: data[subjectKey]
+          }
+          await query(updateParams)
+        }
+
+        data[subjectKey] = data[subjectKey].id 
+      }
+    }
+
     const params = {
       entity: 'package',
       queryType: 'update',
-      includes: ['subjects'],
       filter: { id: Number(id) },
-      data: { ...partialUpdate },
-      relations
+      data,
     }
-    const pack = await query({ ...params })
+
+    const pack = await query(params)
 
     return NextResponse.json(pack, { status: 200 })
   } catch (error) {
     console.error('Error updating package partially:', error)
-    return NextResponse.json({ error: 'Error updating package partially' }, { status: 500 })
+    return NextResponse.json({ error: error.message }, { status: 500 })
   }
 }
 
 
-export const DELETE = async (request, { params }) => {
+export const DELETE = async ({ params }) => {
   const { id } = params
 
   try {
     const params = {
       entity: 'package',
       queryType: 'delete',
-      includes: ['subjects'],
       filter: { id: Number(id) },
     }
 
-    const pack = await query({ ...params })
+    const pack = await query(params)
     
-    return NextResponse.json(pack, { message: 'Deleting package successfully' }, { status: 200 })
+    return NextResponse.json(pack, { status: 200 })
   } catch (error) {
     console.error('Error deleting package:', error)
-    return NextResponse.json({ error: 'Error deleting package' }, { status: 500 })
+    return NextResponse.json({ error: error.message }, { status: 500 })
   }
 }
--- src/app/api/package/route.js
+++ src/app/api/package/route.js
@@ -1,55 +1,60 @@
 import { NextResponse } from 'next/server'
-import query from '~/libs/query'
+import query from '~/app/api/libs/query'
+import { packageShape } from '~/app/api/utils/shapes'
 
 export const POST = async request => {
   try {
-    const { name, description, images, videos, limit, subjects } = await request.json()
-
-    if(!name || !description || !images || !videos || !limit || !subjects){
+    const data = await request.json()
+    
+    if (!packageShape().every(key => key in data)) {
       return NextResponse.json({ error: 'Invalid input' }, { status: 400 })
+    }
+   
+    for (let i = 1; i <= 3; i++) {
+      const subjectKey = `subject${i}`
+      if (!data[subjectKey].id) {
+        const subjectParams = {
+          entity: 'subject',
+          queryType: 'create',
+          data: { ...data[subjectKey] }
+        }
+
+        const newSubject = await query(subjectParams)
+        data[subjectKey] = newSubject.id 
+
+      }else{
+        data[subjectKey] = data[subjectKey].id 
+      }
     }
 
     const params = {
       entity: 'package',
       queryType: 'create',
-      includes: ['subjects'],
-      data: {
-        name,
-        description,
-        images,
-        videos,
-        limit,
-      },
-      relations: [{
-        entity: 'subjects',
-        data: subjects
-      }]
+      data
     }
-
-
-    const newPackage = await query({ ...params })
+    
+    const newPackage = await query(params)
 
     return NextResponse.json(newPackage, { status: 201 })
   } catch (error) {
     console.error('Package creation failed:', error)
-    return NextResponse.json({ error: 'Package creation failed' }, { status: 500 })
+
+    return NextResponse.json({ error: error.message }, { status: 500 })
   }
 }
 
 export const GET = async () => {
   try{
-
     const params = {
       entity: 'package',
       queryType: 'findMany',
-      includes: ['subjects']
     }
 
-    const packages = await query({ ...params })
+    const packages = await query(params)
 
     return NextResponse.json(packages, { status: 200 })
   } catch (error) {
     console.error('Error fetching packages:', error)
-    return NextResponse.json({ error: 'Error fetching packages' }, { status: 500 })
+    return NextResponse.json({ error: error.message }, { status: 500 })
   }
 }
