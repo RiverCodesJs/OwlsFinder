92c8532d9eec8532ca9d70bc39de24b7e4d00f19
Salvador511 <salvadororozcogon511@gmail.com>
2024-10-08 11:15:25 -0600

    Fix: fixed setupTest adding just import needed

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,12 +1,1 @@
 import '@testing-library/jest-dom'
-import { vi } from 'vitest'
-
-vi.mock('next/server', () => ({
-  NextResponse: {
-    json: vi.fn(),
-  },
-}))
-
-vi.mock('~/libs/query', () => ({
-  default: vi.fn(),
-}))
