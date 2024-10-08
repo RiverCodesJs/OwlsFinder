7164ac5e258ddf079b6444c0ce0b2e58399dd6ca
Salvador511 <salvadororozcogon511@gmail.com>
2024-10-08 11:22:30 -0600

    test of the function getOptions which is inside of query

---
+++ src/app/api/libs/__tests__/getOptions.test.js
@@ -0,0 +1,166 @@
+import { describe, it, expect } from 'vitest'
+import { getOptions } from '~/app/api/libs/query'
+
+describe('getOptions libs', () =>{
+  it.each([
+    {
+      descr: 'Empty data',
+      filter: null, 
+      includes: null, 
+      data: null, 
+      relatrions: null,
+      result: {}
+    },
+    {
+      descr: 'Has just filter',
+      filter: { id: 1 }, 
+      includes: null, 
+      data: null, 
+      relations: null,
+      result: { where: { id: 1 } }
+    },
+    {
+      descr: 'Has just includes',
+      filter: null, 
+      includes: ['subjects'], 
+      data: null, 
+      relations: null,
+      result: { 
+        include: {
+          subjects: {
+            select:{
+              id: true,
+            }
+          }
+        } 
+      }
+    },
+    {
+      descr: 'Has just data',
+      filter: null, 
+      includes: null, 
+      data: {
+        name: 'Salvador',
+        lastName: 'Orozco'
+      }, 
+      relations: null,
+      result: { 
+        data: {
+          name: 'Salvador',
+          lastName: 'Orozco'
+        }
+      } 
+    },
+    {
+      descr: 'Has data, includes and relaions',
+      filter: null, 
+      includes: ['subjects'], 
+      data: {
+        name: 'Salvador',
+        lastName: 'Orozco'
+      }, 
+      relations: [
+        {
+          entity: 'subjects',
+          data: [
+            {
+              id: 1,
+              name: 'Math',
+              description: 'Description of Math Subject'
+            },
+            {
+              name: 'Calculus',
+              description: 'Description of Calculus Subject'
+            },
+          ]
+        }
+      ],
+      result: { 
+        data: {
+          name: 'Salvador',
+          lastName: 'Orozco',
+          subjects: {
+            connectOrCreate: [
+              {
+                create:  {
+                  id: 1,
+                  name: 'Math',
+                  description: 'Description of Math Subject'
+                },
+                where:  {
+                  id: 1,
+                },
+              },
+              {
+                create:  {
+                  name: 'Calculus',
+                  description: 'Description of Calculus Subject'
+                },
+                where:  {
+                  id: 0,
+                },
+              },
+            ],
+          },
+        },
+        include: {
+          subjects: {
+            select:{
+              id: true,
+            }
+          }
+        },
+      } 
+    },
+    {
+      descr: 'Has data, includes and one relaion',
+      filter: null, 
+      includes: ['subjects', 'permissions'], 
+      data: {
+        name: 'Salvador',
+        lastName: 'Orozco'
+      }, 
+      relations: [
+        {
+          entity: 'subjects',
+          data: {
+            name: 'Math',
+            description: 'Description of Math Subject'
+          },
+        }
+      ],
+      result: { 
+        data: {
+          name: 'Salvador',
+          lastName: 'Orozco',
+          subjects: {
+            connectOrCreate:{
+              create:  {
+                name: 'Math',
+                description: 'Description of Math Subject'
+              },
+              where:  {
+                id: 0,
+              },
+            },
+          },
+        },
+        include: {
+          subjects: {
+            select:{
+              id: true,
+            }
+          },
+          permissions:{
+            select:{
+              name: true,
+            }
+          }
+        },
+      } 
+    },
+
+  ])('$descr', ({ filter, includes, data, relations, result }) => {
+    expect(getOptions({ filter, includes, data, relations })).toEqual(result)
+  })
+})
