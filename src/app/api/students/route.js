1f037a3c85fb23bc484254ba8b48a6f169b96803
Salvador511 <salvadororozcogon511@gmail.com>
2024-11-02 11:08:26 -0600

    feat: endpoint of students and its test

---
+++ src/app/api/students/route.js
@@ -0,0 +1,40 @@
+import { NextResponse } from 'next/server'
+import { authenticateToken } from '~/app/api/libs/auth'
+import { Students } from '~/app/api/entities'
+import csvFormatter from '~/app/api/students/utils/csvFormatter'
+import ERROR from '~/error'
+import Papa from 'papaparse'
+import query from '~/app/api/libs/query'
+import getPermissionsByEntity from '~/app/api/libs/getPermissionsByEntity'
+
+export const POST = async request => {
+  try {
+    const userId = authenticateToken(request)
+    const { permissions } = await query({
+      entity: 'user',
+      queryType: 'findUnique',
+      filter: { id: Number(userId) },
+      includes: ['permissions']
+    })
+    const hasPermission = getPermissionsByEntity({ permissions, entity: Students, action: 'create' })
+    if(hasPermission){
+      const csvFile = await request.text()
+      if(!csvFile) return ERROR.INVALID_FIELDS()
+      const { data } = Papa.parse(csvFile, {
+        header: true,
+        skipEmptyLines: true,
+      })
+      const processedData = csvFormatter(data)
+      const response = await query({
+        entity: 'user',
+        queryType: 'createMany',
+        data: processedData,
+      })
+      return NextResponse.json(response, { status: 201 })
+    } else {
+      return ERROR.FORBIDDEN()
+    }
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: error.status || 500 })
+  }
+}
