0db1254a21f08c3cd54af98b5f9890ef8575d1f4
Salvador511 <salvadororozcogon511@gmail.com>
2024-11-14 16:58:45 -0600

    feat: test of validatePermission libs

---
+++ src/app/api/libs/__tests__/validatePermission.test.js
@@ -0,0 +1,45 @@
+import { describe, it, expect, vi } from 'vitest'
+import validatePermission from '~/app/api/libs/validatePermission'
+
+vi.mock('~/app/api/libs/auth', () => {
+  return { authenticateToken: () => 1 }
+})
+vi.mock('~/app/api/libs/queryDB', () => {
+  return {
+    default: () => ({
+      permissions: [
+        { name: 'action_any' }
+      ]
+    })
+  }
+})
+describe('validatePermission libs', () =>{
+  it.each([
+    {
+      descr: 'The user has the permission',
+      entity: {
+        name: 'any',
+        permissions: {
+          action: true
+        }
+      },
+      action: 'action',
+      request: 'request',
+      result: true
+    },
+    {
+      descr: 'The user has not the permission',
+      entity: {
+        name: 'any',
+        permissions: {
+          otherAction: true
+        }
+      },
+      action: 'otherAction',
+      request: 'request',
+      result: false
+    }
+  ])('$descr', async ({ result, ...params }) => {
+    expect(await validatePermission({ ...params })).toEqual(result)
+  })
+})
