bcb098b157a2636d5ada5b75af080c854fca7e60
OmarMol75 <gaelmolinareza@gmail.com>
2025-06-02 22:58:17 -0600

    feat: added success mode to verify page

--- src/app/verify/page.jsx
+++ src/app/verify/page.jsx
@@ -4,6 +4,9 @@ import Image from "next/image"
 import { styled } from '@mui/material/styles'
 import getClassPrefixer from "../UI/classPrefixer"
 import { images } from "../images"
+import useToken from "../store/useToken"
+import Link from "next/link"
+import { useStore } from "zustand"
 
 const displayName = 'VerifyEmail'
 const classes = getClassPrefixer(displayName)
@@ -34,10 +37,25 @@ const Container = styled('div')(({ theme }) => ({
 }))
 
 const Verify = () => {
+  const token = useStore(useToken, (state) => state.token)
+
+  if(token) {
+    return (
+      <Container>
+        <Stack className={classes.content_box} spacing={3}>
+          <Image src={images.buhos_logo} width={330} height={250} alt="Buho's Logo"/>
+          <Typography variant="h2">Verifica tu cuenta</Typography>
+          <Typography variant="body1">Para comenzar a utilizar todas las herramientas y recursos disponibles, por favor, confirma tu cuenta haciendo clic en el botón de abajo.</Typography>
+          <Link href="/students"><Button className={classes.access_button}>Ingresar</Button></Link>
+        </Stack>
+      </Container>
+    )
+  }
+
   return(
     <Container>
       <Stack className={classes.content_box} spacing={3}>
-        <Image src={images.buhos_logo} width={330} height={250}/>
+        <Image src={images.buhos_logo} width={330} height={250} alt="Buho's Logo"/>
         <Typography variant="h2">Verifica tu correo</Typography>
         <Typography variant="body1">Un correo ha sido enviado a tu cuenta. Revísalo para acceder al proceso de selección</Typography>
       </Stack>
