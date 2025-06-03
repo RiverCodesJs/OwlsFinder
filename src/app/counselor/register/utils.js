22fbbfba9bbb4e39ed35a7e1d36ca1e78d8e3ba5
OmarMol75 <gaelmolinareza@gmail.com>
2025-06-02 22:48:29 -0600

    feat: added function to format payload of the register form

--- src/app/counselor/register/utils.js
+++ src/app/counselor/register/utils.js
@@ -1,12 +1,24 @@
 import * as Yup from 'yup'
 
 const registerSchema = Yup.object({
-  firstName: Yup.string().required("Ingrese su nombre"),
-  fatherName: Yup.string().required('Ingresa sus apellidos'),
-  motherName: Yup.string().required('Ingresa sus apellidos'),
+  names: Yup.string().required("Ingrese su nombre"),
+  paternalSurname: Yup.string().required('Ingresa sus apellidos'),
+  maternalSurname: Yup.string().required('Ingresa sus apellidos'),
   password: Yup.string().required('Ingresa una nueva contraseña'),
   repeatPass: Yup.string().required('Ingresa de nuevo la nueva contraseña'),
 })
 
+const formatPayload = (values) => ({
+  names: values.names,
+  paternalSurname: values.paternalSurname,
+  maternalSurname: values.paternalSurname,
+  email: `${values.names[0]}${values.paternalSurname}@cobachih.edu.mx`.toLowerCase(),
+  password: values.password,
+  enrollmentId: "",
+  groups: [],
+  currentGroup: "",
+  nextGroup: "",
+  shift: "",
+})
 
-export default registerSchema
+export {registerSchema, formatPayload}
