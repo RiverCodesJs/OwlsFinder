ae3dabf14134fee8d847378b390189b976d626f3
OmarMol75 <gaelmolinareza@gmail.com>
2025-06-02 23:48:08 -0600

    fix: changed token validation to useParams

--- src/app/forgot/[token]/page.jsx
+++ src/app/forgot/[token]/page.jsx
@@ -1,14 +1,12 @@
 'use client'
 import { useQueryClient } from "@tanstack/react-query"
-import { useRouter } from "next/navigation"
+import { useParams, useRouter } from "next/navigation"
 import { Suspense, useEffect } from "react"
 import useToken from "~/app/store/useToken"
 import Loading from "~/app/UI/shared/Loading"
 
 const ForgotRedirect = ({params}) => {
-
-
-  const { token } = params
+  const token = useParams()
   const router = useRouter()
   const client = useQueryClient()
   const { setToken } = useToken()
