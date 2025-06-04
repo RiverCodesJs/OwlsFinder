0c41c9e6374efc459c8e994bb692bee8f7e5ffb7
OmarMol75 <gaelmolinareza@gmail.com>
2025-06-03 18:05:36 -0600

    refactor: changed export syntaxis in reset password utils

--- src/app/forgot/success/utils.js
+++ src/app/forgot/success/utils.js
@@ -1,13 +1,11 @@
 import * as Yup from 'yup'
 
-const getForgotValues = () => ({
+export const getForgotValues = () => ({
   password: '',
   repeatPass: '',
 })
 
-const getForgotSchema = () => Yup.object({
+export const getForgotSchema = () => Yup.object({
   password: Yup.string().required('Ingresa una nueva contraseña'),
   repeatPass: Yup.string().required('Ingresa de nuevo la nueva contraseña'),
 })
-
-export {getForgotSchema, getForgotValues}
