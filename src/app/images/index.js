1c7b469a68825ebd773d5d04511d83deba990564
OmarMol75 <gaelmolinareza@gmail.com>
2025-06-04 17:18:13 -0600

    chore: change variable names to camelCase

--- src/app/images/index.js
+++ src/app/images/index.js
@@ -1,4 +1,4 @@
 export const images = {
-  buhos_logo: "https://res.cloudinary.com/dshor6wna/image/upload/v1660262115/logo_completo_jb3i32.png",
+  buhosLogo: "https://res.cloudinary.com/dshor6wna/image/upload/v1660262115/logo_completo_jb3i32.png",
   buho: "https://res.cloudinary.com/dshor6wna/image/upload/v1660262113/buho_vdjcgr.png"
 }
