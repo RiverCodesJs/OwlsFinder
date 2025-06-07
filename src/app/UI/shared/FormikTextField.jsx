cc510f4555cca7e51bac6b96aeda083a2fa8ea1f
OmarMol75 <gaelmolinareza@gmail.com>
2025-06-07 11:56:26 -0600

    chore (formikTextField): improved code syntaxis to line up with eslint

--- src/app/UI/shared/FormikTextField.jsx
+++ src/app/UI/shared/FormikTextField.jsx
@@ -1,41 +1,38 @@
-import { Stack, TextField, Typography } from "@mui/material"
-import { styled } from "@mui/material/styles"
-import { useFormikContext } from "formik"
-import { useState } from "react"
+import { Stack, TextField } from '@mui/material'
+import { styled } from '@mui/material/styles'
+import { useFormikContext } from 'formik'
 
-const CustomField = styled(TextField)(({theme}) => ({
+const CustomField = styled(TextField)(({ theme }) => ({
   backgroundColor: theme.palette.contrast.main,
-  borderRadius: "4px",
-  [`& .MuiFilledInput-input`]: {
-      padding: "16px 12px"
+  borderRadius: '4px',
+  ['& .MuiFilledInput-input']: {
+    padding: '16px 12px'
   },
-  [`& .MuiFilledInput-root`]: {
-      padding: "0"
+  ['& .MuiFilledInput-root']: {
+    padding: 0
   },
 }))
 
-const FormikTextField = ({ variant = "filled" , field, password, ...props}) => {
+const FormikTextField = ({ variant = 'filled' , field, password, ...props }) => {
 
-  const [visibility, setVisibility] = useState(false)
-  const {errors, setFieldValue} = useFormikContext()
+  const { errors, setFieldValue } = useFormikContext()
 
   return (
     <Stack width="100%" alignItems="center">
       <CustomField 
-      {...props}
-      placeholder={props.placeholder}
-      error={Boolean(errors[field?.name])}
-      name={field?.name}
-      variant={variant}
-      value={field?.value ?? ''}
-      onChange={({target}) => setFieldValue(field?.name, target.value)}
-      slotProps={password ? {
-        input: { 
-          disableUnderline: true,
-          type: password && !visibility ? 'password' : 'text', 
+        {...props}
+        placeholder={props.placeholder}
+        error={Boolean(errors[field?.name])}
+        name={field?.name}
+        variant={variant}
+        value={field?.value ?? ''}
+        onChange={({ target }) => setFieldValue(field?.name, target.value)}
+        slotProps={password ? {
+          input: { 
+            disableUnderline: true,
           }
         }: null
-      } 
+        } 
       />
       {/*errors[field?.name] ? <Typography width="60%" color="error" fontSize={12} textAlign="left">{errors[field?.name]}</Typography> : null*/}
     </Stack>
