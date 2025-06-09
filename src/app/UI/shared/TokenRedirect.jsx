85cfb8f130316cc49fd75a06080d5ec6fd9a3c51
OmarMol75 <gaelmolinareza@gmail.com>
2025-06-09 16:15:02 -0600

    refactor: improve tokenRedirect for token validation by the API

--- src/app/UI/shared/TokenRedirect.jsx
+++ src/app/UI/shared/TokenRedirect.jsx
@@ -1,25 +1,49 @@
 import { useQueryClient } from '@tanstack/react-query'
 import { useParams, useRouter } from 'next/navigation'
 import { useEffect } from 'react'
+import { useApiQuery } from '~/app/Lib/apiFetch'
+import { useData } from '~/app/store/useData'
 
 import useToken from '~/app/store/useToken'
+import Loading from './Loading'
 
 export const TokenRedirect = ({ route }) => {
 
   const { token } = useParams()
+  const { setToken } = useToken()
+  const { setUserId } = useData()
   const router = useRouter()
   const client = useQueryClient()
-  const { setToken } = useToken() 
-
+  const { data, isLoading, error } = useApiQuery({ path: 'verify' })
+  const { data:meData } = useApiQuery({ path: 'me' })
+  
   useEffect(() => {
-    if (token) {
+    if(token) {
       setToken(token)
+    }
+    if(data?.userId) {
+      if(meData?.type === 'Counselor') {
+        setUserId(data?.userId)
+      }
       router.replace(`/${route}`)
-    } else {
+    }
+    if(error) {
       client.clear()
       router.replace('/login')
     }
-  }, [token, router, route, client, setToken])
+  }, [
+    meData,
+    data, 
+    error, 
+    token, 
+    route, 
+    router, 
+    setUserId, 
+    client, 
+    setToken
+  ])
+
+  if(isLoading) return <Loading/>
 
   return null
 }
