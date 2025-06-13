141697957de142fbe4a4437f5ed5f49f9121e011
OmarMol75 <gaelmolinareza@gmail.com>
2025-06-12 18:21:49 -0600

    feat (api): include role in token validation

--- src/app/api/verify/route.js
+++ src/app/api/verify/route.js
@@ -2,11 +2,12 @@ import { NextResponse } from 'next/server'
 import { authenticateToken } from '~/app/api/libs/auth'
 
 import ERROR from '~/error'
+
 export const GET = async request => {
   try {
-    const userId = authenticateToken(request)
-    if (!userId) return ERROR.FORBIDDEN()
-    return NextResponse.json({ userId }, { status: 200 })
+    const { userId, role } = authenticateToken(request)
+    if (!userId || !role) return ERROR.FORBIDDEN()
+    return NextResponse.json({ userId, role }, { status: 200 })
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: error.status || 401 })
   }
