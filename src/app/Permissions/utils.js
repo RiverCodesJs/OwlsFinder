7c9fd8073515131aa8c78524075ad46a2ab31aa8
OmarMol75 <gaelmolinareza@gmail.com>
2025-06-12 21:36:37 -0600

    feat (Permissions): implement role validation in usePermitted

--- src/app/Permissions/utils.js
+++ src/app/Permissions/utils.js
@@ -1,11 +1,17 @@
 import { useData } from '../store/useData'
 import useToken from '../store/useToken'
 
-const usePermitted = () => {
+const isValidRole = ({ role, requiredRole }) => {
+  if (role === 'Admin') return true
+  if (role === requiredRole) return true
+  return false
+}
+
+const usePermitted = ({ requiredRole = 'Admin' }) => {
   const { token } = useToken()
   const { userId, role } = useData()
 
-  if (token && userId && role) {
+  if (token && userId && isValidRole(role, requiredRole)) {
     return true
   }
   return false
