cb7c4a9d9b6fd0b7496a21e55882e42fec296df0
OmarMol75 <gaelmolinareza@gmail.com>
2025-06-12 18:55:36 -0600

    feat (auth): include role validation in auth utility

--- src/app/api/libs/auth.js
+++ src/app/api/libs/auth.js
@@ -6,8 +6,8 @@ export const authenticateToken = ({ headers }) => {
   const authHeader = headers.get('authorization')
   const token = authHeader && authHeader.replace(/Bearer /,'')
   if (token) {
-    const { userId } = jwt.verify(token, process.env.JWT_SECRET)
-    return userId
+    const { userId, role } = jwt.verify(token, process.env.JWT_SECRET)
+    return { userId, role }
   } else {
     ERROR.FORBIDDEN()
   }
