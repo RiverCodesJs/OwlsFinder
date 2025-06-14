37b29f5676f678aa5fd42cb6ec4faaa4c3fe39a5
OmarMol75 <gaelmolinareza@gmail.com>
2025-06-14 10:38:36 -0600

    feat (login): implement clsx for dynamic classnames and make more declarative variables

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -3,6 +3,7 @@ import { Snackbar, Stack, Typography as T } from '@mui/material'
 import { styled } from '@mui/material/styles'
 import { useState } from 'react'
 import Image from 'next/image'
+import classNames from 'clsx'
 
 import getClassPrefixer from '~/app/UI/classPrefixer'
 
@@ -10,24 +11,12 @@ import { ProfessorForm } from './components/ProfessorForm'
 import { StudentsForm } from './components/StudentsForm'
 import { buho, buhosLogo } from '../images'
 
-const displayName = 'login'
+const displayName = 'Login'
 const classes = getClassPrefixer(displayName)
 
 const Container = styled('div')(({ theme }) => ({
   height: '100vh',
-  [`& .${classes.focusedContainer}`]: {
-    width: '50vw',
-    height: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    transition: 'all 0.3s ease',
-    gap: '1ch',
-    backgroundColor: theme.palette.primary.main,
-    color: theme.palette.contrast.main
-  },
-  [`& .${classes.unfocusedContainer}`]: {
+  [`& .${classes.activeContainer}`]: {
     width: '50vw',
     height: '100vh',
     display: 'flex',
@@ -39,6 +28,18 @@ const Container = styled('div')(({ theme }) => ({
     backgroundColor: theme.palette.contrast.main,
     color: theme.palette.primary.main
   },
+  [`& .${classes.inactiveContainer}`]: {
+    width: '50vw',
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    transition: 'all 0.3s ease',
+    gap: '1ch',
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.contrast.main
+  },
 }))
 
 const Login = ({ 
@@ -46,32 +47,38 @@ const Login = ({
   setSnackbarMessage, 
 }) => {
 
-  const [focused, setFocused] = useState(true)
+  const [activeScreen, setActiveScreen] = useState(true)
 
   return (
     <Container>
       <Stack direction="row">
         <div 
-          className={focused ? classes.unfocusedContainer : classes.focusedContainer}
-          onFocus={() => setFocused(true)}
+          className={classNames({
+            [classes.activeContainer]: activeScreen,
+            [classes.inactiveContainer]: !activeScreen
+          })}
+          onFocus={() => setActiveScreen(true)}
         >
-          {focused 
+          {activeScreen 
             ? <Image src={buhosLogo} width={250} height={180} alt="Owls Logo"/> 
             : null}
           <T variant="h3">Inicio de sesión</T>
           <T variant="body1">Si ya tienes una cuenta</T>
-          <ProfessorForm setSnackbarMessage={setSnackbarMessage} focused={focused}/>
+          <ProfessorForm setSnackbarMessage={setSnackbarMessage} isActive={activeScreen}/>
         </div>
         <div 
-          className={focused ? classes.focusedContainer : classes.unfocusedContainer}
-          onFocus={() => setFocused(false)}
+          className={classNames({
+            [classes.activeContainer]: !activeScreen,
+            [classes.inactiveContainer]: activeScreen
+          })}
+          onFocus={() => setActiveScreen(false)}
         >
-          {!focused 
+          {!activeScreen 
             ? <Image src={buho} width={200} height={100} alt="Owls Logo"/> 
             : null}
           <T variant="h3">Alumnos</T>
           <T variant="body1">Verifica tu informacion</T>
-          <StudentsForm setSnackbarMessage={setSnackbarMessage} focused={focused}/>
+          <StudentsForm setSnackbarMessage={setSnackbarMessage} isActive={activeScreen}/>
         </div>
         <Snackbar
           open={Boolean(snackbarMessage)}
