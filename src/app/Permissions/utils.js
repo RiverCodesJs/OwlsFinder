7cb4eb21281500ce6c5ad37df33712829c93e722
OmarMol75 <gaelmolinareza@gmail.com>
2025-06-14 11:40:01 -0600

    refactor (usePermitted): change all role mentions to 'type'

--- src/app/Permissions/utils.js
+++ src/app/Permissions/utils.js
@@ -1,17 +1,17 @@
 import { useData } from '../store/useData'
 import useToken from '../store/useToken'
 
-const isValidRole = ({ role, requiredRole }) => {
-  if (role === 'Admin') return true
-  if (role === requiredRole) return true
+const isValidType = ({ type, requiredType }) => {
+  if (type === 'Admin') return true
+  if (type === requiredType) return true
   return false
 }
 
-const usePermitted = ({ requiredRole = 'Admin' }) => {
+const usePermitted = ({ requiredType = 'Admin' }) => {
   const { token } = useToken()
-  const { userId, role } = useData()
+  const { userId, type } = useData()
 
-  if (token && userId && isValidRole(role, requiredRole)) {
+  if (token && userId && isValidType(type, requiredType)) {
     return true
   }
   return false
