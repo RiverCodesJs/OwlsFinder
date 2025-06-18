9d0b310a6ddc0a50ee6729b0b25812d4ff488563
OmarMol75 <gaelmolinareza@gmail.com>
2025-06-18 16:47:00 -0600

    feat (counselor): add counselor creation form as a button in UI

---
+++ src/app/counselor/page.jsx
@@ -0,0 +1,82 @@
+'use client'
+import { AddSharp, CalendarTodaySharp } from '@mui/icons-material'
+import { IconButton, Modal, Snackbar, Stack, Typography } from '@mui/material'
+import { styled } from '@mui/material/styles'
+import getClassPrefixer from '../UI/classPrefixer'
+import { Permitted } from '../Permissions/Permitted'
+import { NotAvailable } from '../UI/shared/NotAvailable'
+import { useState } from 'react'
+import CounselorCreationForm from './components/CounselorCreationForm'
+
+const displayName = 'PanelCounselor'
+const classes = getClassPrefixer(displayName)
+
+const Container = styled('div')(({ theme }) => ({
+  width: '100%',
+  height: '100vh',
+  padding: '2rem',
+  [`& .${classes.iconButton}`]: {
+    fill: theme.palette.primary.main,
+    fontSize: '2.5rem'
+  }
+}))
+
+const PanelCounselor = ({
+  openCounselorCreateModal,
+  setOpenCounselorCreateModal,
+  snackbarMessage,
+  setSnackbarMessage
+}) => {
+  return (
+    <Container>
+      <Stack>
+        <Stack direction="row" justifyContent="space-between" alignItems="center" width="100%">
+          <Typography variant="h2" fontWeight="bold">Clases</Typography>
+          <Stack direction="row" justifyContent="right">
+            <IconButton onClick={() => setOpenCounselorCreateModal(true)}>
+              <AddSharp className={classes.iconButton}/>
+            </IconButton>
+            <IconButton>
+              <CalendarTodaySharp className={classes.iconButton}/>
+            </IconButton>
+          </Stack>
+        </Stack>
+      </Stack>
+      <Modal
+        open={openCounselorCreateModal}
+        onClose={() => setOpenCounselorCreateModal(false)}
+      >
+        <CounselorCreationForm 
+          onClose={() => setOpenCounselorCreateModal(false)}
+          setSnackbarMessage={setSnackbarMessage}
+        />
+      </Modal>
+      <Snackbar
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+        open={Boolean(snackbarMessage)}
+        autoHideDuration={4000}
+        onClose={() => setSnackbarMessage(null)}
+        message={snackbarMessage}
+      />
+    </Container>
+  )
+}
+
+const Wrapper = () => {
+
+  const [openCounselorCreateModal, setOpenCounselorCreateModal] = useState(false)
+  const [snackbarMessage, setSnackbarMessage] = useState(null)
+
+  return (
+    <Permitted Fallback={NotAvailable} requiredType='Counselor'>
+      <PanelCounselor
+        openCounselorCreateModal={openCounselorCreateModal}
+        setOpenCounselorCreateModal={setOpenCounselorCreateModal}
+        snackbarMessage={snackbarMessage}
+        setSnackbarMessage={setSnackbarMessage}
+      />
+    </Permitted>
+  )
+}
+
+export default Wrapper
