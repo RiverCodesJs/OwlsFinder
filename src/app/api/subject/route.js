cfea67dfee94e6836562fc056c0b985c3acac62c
OmarMol75 <gaelmolinareza@gmail.com>
2025-07-10 16:17:48 -0600

    fix (api/subject): set action in validatePermission to all caps and await params

--- src/app/api/subject/route.js
+++ src/app/api/subject/route.js
@@ -9,7 +9,7 @@ import payloadFormatter from '~/app/api/utils/payloadFormatter'
 
 export const POST = async request => {
   try {
-    const hasPermission = await validatePermission({ entity: Subject, action: 'create', request })
+    const hasPermission = await validatePermission({ entity: Subject, action: 'CREATE', request })
     const data = await request.json()
     if(hasPermission && validatorFields({ data, shape: Subject.shape })){
       const payload = await queryDB({
