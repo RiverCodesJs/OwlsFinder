9d77331fe23252998b5204268ad9dae4e01a7b99
OmarMol75 <gaelmolinareza@gmail.com>
2025-07-22 13:12:22 -0600

    fix: enhance validatorFields to support optional keys

--- src/app/api/libs/validatorFields.js
+++ src/app/api/libs/validatorFields.js
@@ -1,7 +1,11 @@
 import ERROR from '~/error'
 
-const validatorFields = ({ data, shape }) => {
-  if(!shape.every(key => key in data)) return ERROR.INVALID_FIELDS()
+const validatorFields = ({ data, shape, optionals = [] }) => {
+  const newShape = shape.reduce((acc, key) => {
+    if(optionals.includes(key)) return [...acc]
+    return [...acc, key]
+  }, [])
+  if(!newShape.every(key => key in data)) return ERROR.INVALID_FIELDS()
   return true
 }
 
